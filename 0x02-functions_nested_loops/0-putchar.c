5032a966ebf532fe1ece04b047eb614dd9cba809
olohitai <olohitaipeace@gmail.com>
2022-03-15 07:09:31 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,14 +9,15 @@
  */
 int main(void)
 {
-	char text[7] = "_putchar";
-	int i;
-
-	for (i = 0; i < 7; i++)
-	{
-	_putchar(text[i]);
-	}
-_putchar('\n')
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
 
