8c633f9461a4128aefb03604f1e11bc667aa0bbc
olohitai <olohitaipeace@gmail.com>
2022-03-17 12:04:37 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; 1++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
@@ -29,7 +29,7 @@ int main(void)
 			printf("%d", i);
 		else
 		{
-			printf("%d", i);
+			printf(" %d", i);
 		}
 	}
 	printf("\n");
